0988813055be91fc1599cc49af123be9bac5f4c6
Oleg Vasilevsky <89103905+LEGO23@users.noreply.github.com>
2022-10-10 14:49:38 +0300

    Add files via upload

---
+++ crud/config/connect.php
@@ -0,0 +1,7 @@
+<?php
+    $connect = mysqli_connect('localhost', 'root', '', 'crud');
+
+    if (!$connect) {
+      die('Error');
+    }
+?>
---
+++ crud/vendor/delete.php
@@ -0,0 +1,7 @@
+<?php
+  require_once '../config/connect.php';
+  $id = $_GET['id'];
+
+  mysqli_query($connect, "DELETE FROM `goods` WHERE `goods`.`id` = '$id'");
+  header('Location: /');
+?>
